bd75559cdecc0655a1250db6c3fce6afeff9ed97
unknown <victorjames40v@gmail.com>
2023-08-20 01:58:51 +0100

    wip:worked on the page route -installed react router dom -worked on routing the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
+
+import {
+  NavLink,
+  createBrowserRouter,
+  Routes,
+  Route,
+  RouterProvider,
+  createRoutesFromElements
+} from "react-router-dom"
+import "./index.css"
+// pages
 import Header from "./components/header"
 import HomePage from "./page/homePage"
-import "./index.css"
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+        <Route>
+          <Route path="/" element={<Header />}/>
+           <Route path="/homepage" element={<HomePage/>}></Route>
+        </Route>
+      )
+      
+)
 
 function App() {
 
   return (
-    <div className=''>
-      <Header />
-      <div>
-        <HomePage/>
-      </div>
+    <div>
+      <RouterProvider router={router}/>
     </div>
   )
 }
