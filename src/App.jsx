41e6f183754da1c98e2e58ffff3f7fbad5d560fc
unknown <victorjames40v@gmail.com>
2023-08-25 00:57:31 +0100

    wip: created hiring managers dashboard -created the hiring managers dashbord, hiring managers can now see list of talents available
    close #7

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,15 @@ import Header from "./components/header"
 import HomePage from "./page/Homepage/homePage"
 import JobSeekers from "./page/Jobseeker/jobSeeker"
 import topSearches from "./components/TopSearches/topSearches"
+import { JobGivers } from "./page/jobGiversBoard/jobGiversBoard"
+import UserProfile from "./page/userprofile/userProfile"
+import { ProfilePicture } from "./components/ProfilePicture/profilePictue"
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
         <Route>
-          <Route path="/" element={<JobSeekers/>} />
+          <Route path="/" element={<UserProfile/>} />
       
         </Route>
       )
