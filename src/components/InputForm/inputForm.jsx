dfca0ee3a4fd2ffa135ab01837acdf56951f872b
unknown <victorjames40v@gmail.com>
2023-08-28 12:34:48 +0100

    chore: create user sign up page - imporved the user sign up page by adding the sign up button and agreement checkbox
    closes #10

--- src/components/InputForm/inputForm.jsx
+++ src/components/InputForm/inputForm.jsx
@@ -1,7 +1,7 @@
 export const Inputform = ({ name, details, handleChange,value}) => (
     
-    <div className=" w-[90%]">
+    <div className=" w-[100%]">
         <label className="">{name}:</label>
-        <input name={name} value={value} onChange={handleChange} required className="border-black border p-1 ml-1 rounded-lg md:w-[90%] mb-4 w-[100%] outline-none" />
+        <input name={name} value={value} onChange={handleChange} required className="border-black border p-1 ml-1 rounded-lg md:w-[100%] w-[95%] mb-3 w-[100%] outline-none" />
     </div>
 )
--- src/page/signUp/signupPage.jsx
+++ src/page/signUp/signupPage.jsx
@@ -1,3 +1,4 @@
+import { CustomButton } from "../../components/CustomButtons/customButton"
 import { useState } from "react"
 import { Inputform } from "../../components/InputForm/inputForm"  
 
@@ -19,28 +20,32 @@ export function SignupPage() {
       console.log(myDetails)
     }
     return (
-        <div className=" flex md:justify-center flex-col items-center h-screen pt-3 bg-blue-400 md:w-[50%] w-[100%]">
+        <div className=" flex  flex-col items-center h-screen  bg-blue-400  w-[100%] pt-6 pb-7">
             <h2 className="text-2xl text-white font-bold ">Registration</h2>
             <span className=" text-white">Create an account with us today</span>
-            <form className="md:mt-10 mt-5 flex justify-center items-center flex-col w-[100%]">
+            <form className="md:mt-8 mt-5 flex flex-col md:w-[40%] w-[100%] ">
 
                 <Inputform name="FirstName" details={myDetails} value={myDetails.FirstName} handleChange={HandleChange} />
                 <Inputform name="LastName" details={myDetails} value={myDetails.LastName} handleChange={HandleChange}/> 
                 <Inputform name="Email" details={myDetails} value={myDetails.email} handleChange={HandleChange}/>
                 <Inputform name="Password" details={myDetails} value={myDetails.password} handleChange={HandleChange}/>
                 <Inputform name="ConfirmPassword" details={myDetails} value={myDetails.confirmPassword} handleChange={HandleChange} />
-                {/* <div className=" w-[90%]">
-                    <label className="">Testing:</label>
-                    <input name="testing" value={myDetails.firstName} onChange={HandleChange} required className="border-black border p-1 ml-1 rounded-lg md:w-[90%] mb-4 w-[100%] outline-none" />
-                </div> */}
-                <div className="md:w-[90%] w-[100%] p-1 ml-7">
+                <div className="md:w-[100%] w-[100%] p-1 ">
                     <label>Status:</label>
-                    <select name="status" className="border-black border p-1 ml-1 rounded-lg md:w-[90%] w-[90%] outline-none">
+                    <select name="status" className="border-black border p-1 ml-1 rounded-lg md:w-[100%] w-[90%] outline-none">
                         <option>Jobseeker</option>
                         <option>recruiter</option>
                     </select>
                 </div>
+                 
+                <div className="flex ml-2">
+                    <input type="checkbox" />
+                    <p className="ml-2 my-2">I accept all <a href="#">terms & conditions</a></p>
+                </div>
+
+                <CustomButton>SIGN UP</CustomButton>
             </form>
+            <p>Already Have An Account ? <a href="#" className="text-red-800 pointer">Login Now</a></p>
          </div>
     )
 }
